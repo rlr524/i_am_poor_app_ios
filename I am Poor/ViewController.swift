acc18a2dc0bc024032c3d81b3a7cf029b8bcdcb5
Rob Ranf <robranf@gmail.com>
2020-01-08 17:41:22 -0800

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Rob Ranf on 1/8/20.
+//  Copyright © 2020 Rob Ranf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
